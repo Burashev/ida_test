40c14a3e378103fafde1978201b01b16c84cbddd
Burashev Shamil <shburashev@ya.ru>
2022-03-15 20:01:43 +0300

    add: Source Sans Pro font

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, voluptates?</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Fonts */
+@font-face {
+  src: url("~@/assets/fonts/SourceSansPro-Regular.ttf");
+  font-family: "Source Sans Pro";
+  font-weight: 400;
 }
+
+@font-face {
+  src: url("~@/assets/fonts/SourceSansPro-SemiBold.ttf");
+  font-family: "Source Sans Pro";
+  font-weight: 600;
+}
+
+@font-face {
+  src: url("~@/assets/fonts/SourceSansPro-Bold.ttf");
+  font-family: "Source Sans Pro";
+  font-weight: 700;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Source Sans Pro", sans-serif;
+}
+</style>
+
+<style scoped>
+
 </style>
