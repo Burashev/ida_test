59bcf16c6c0d5aa78d652ade94b80941c69cd352
Burashev Shamil <shburashev@ya.ru>
2022-03-16 15:25:36 +0300

    add: ProductItem&ProductList components and vuex product module

---
+++ src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="items-list">
+    <product-item v-for="product in products" :product="product" :key="product.id"/>
+  </div>
+</template>
+
+<script>
+import ProductItem from '@/components/ProductItem'
+
+export default {
+  name: 'ProductList',
+  components: { ProductItem },
+  props: {
+    products: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.items-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 16px;
+}
+</style>
